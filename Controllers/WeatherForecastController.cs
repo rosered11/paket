d1444b8c3ac7328a5220bc0dfc8a596fe88989cd
krihsana n <krihsana.n@appman.co.th>
2020-08-23 16:14:26 +0700

    Initial project

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace paket.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var test = new WeatherForecast{
+                Date = DateTime.Now,
+                TemperatureC = 0,
+                Summary = "Haha"
+            };
+            return JsonConvert.SerializeObject(test);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # paket
+
+## Install and restore Paket in local
+
+```
+dotnet new tool-manifest
+dotnet tool install paket
+dotnet tool restore
+```
+
+## Initialize Paket by creating a dependencies file
+
+```
+dotnet paket init
+```
+
+## Add all packages for used
+
+update in file `paket.dependencies`
+
+## Add Create file paket.references per-project
+
+create file `paket.references` and set the packages use in project
+
+## Install package in paket.dependencies
+
+```
+dotnet paket install
+```
+
+## Update Paket
+
+dotnet paket update
